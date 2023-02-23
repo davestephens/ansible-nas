691499112db0902a55bb9af847f44f19303414f9
Anarion <anarion80@gmail.com>
2023-02-23 17:11:08 +0100

    :bug: Fix phpmyadmin config template

--- roles/phpmyadmin/templates/config.inc.php
+++ roles/phpmyadmin/templates/config.inc.php
@@ -7,9 +7,8 @@ $cfg['Servers'] = [
 {% if item.exists|d(False) == True %}
     {{ loop.index }} => [
         'auth_type' => 'cookie',
-        'host' => '{{ item.container.NetworkSettings.IPAddress }}',
+        'host' => '{{ lookup('ansible.builtin.dict', item.container.NetworkSettings.Networks).value.IPAddress }}',
         // TODO: Update to extract actual port
-        // 'port' => '{{ lookup('ansible.builtin.dict', item.container.NetworkSettings.Ports)[0].key | regex_search('.*\\d') }}',
         'port' => '3306',
         'username' => '',
         'password' => '',
